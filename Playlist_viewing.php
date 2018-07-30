645a2f4f0e64ee5e823397f797c90dc69a30d3e7
sayed <sksayed@live.com>
2018-07-30 22:22:17 +0600

    all working so far

--- Playlist_viewing.php
+++ Playlist_viewing.php
@@ -40,21 +40,70 @@
 	<div style="clear: both;">
 		<?php
 		require("mp3file.class.php");
+			
+        require("dbconnection.php");
+
+           $pl_id = $_GET["serial"] ;
+           //echo $pl_id ;
+           
+           $playlist_er_result = all_songs_under_a_playlist ($pl_id) ;
+           if (mysqli_num_rows( $playlist_er_result) > 0 )
+           {
+           	//jodi playlist e gaan thake 
+           	   //playlist er name likhte hbe 
+           	$playlist = p ($pl_id) ;
+           	echo "<h2>".$playlist["playlist_name"]."</h2>";
+           	echo "<ul id='Playlist' align='left'>";
+           	$s = 1 ;
+           	 while ( $q = mysqli_fetch_assoc( $playlist_er_result) ) 
+           	 {
+           	 	# code...
+           	 	  $a= all_values_of_a_song ($q["song_id"]) ;
+           	 	        //$path_parts = pathinfo($a[$i]);
+			 			$name=$a["song_name"]; // file name 
+			 				$mp3file = new MP3File($a["song_path"]);//song path 
+			 				$duration2 = $mp3file->getDuration();
+			 				$dur=MP3File::formatTime($duration2);
+			 			
+			 			
+			 echo "<li class='current-song'><a id='".$name." - ".$dur."' href='".$a["song_path"]."'>".$s.") ".$a["song_name"]." - ".$dur."</a></li>";
+			 			
+			 			
+			 			
+			 $s++;				
+			 			
+           	 }
+
+           }
+           else
+           {
+           	$playlist = p ($pl_id) ;
+           	echo "<h2>".$playlist["playlist_name"]."</h2>";
+           	 echo "<h1>no song in this play list </h1>" ;
+           }
+
+
+
+
+
+
+
 			$sl=0;
-			foreach(glob('uploads/*', GLOB_ONLYDIR) as $dir) 
+			
+			foreach(glob('uploads/*', GLOB_ONLYDIR) as $dir) //sob gula directory nise 
 			{
-				$dirname = basename($dir);
-				if($_GET['serial']==$sl)
+				$dirname = basename($dir); //eta dir er name 
+				if($_GET['serial']==$sl) //playlist ID
 				{
-					echo "<h2>".$dirname."</h2>";	
+					echo "<h2>".$dirname."</h2>";// eta hobe playlist name 	
 					echo "<ul id='Playlist' align='left'>";
-					$a=array_slice(scandir($dir),2) ;
+					$a=array_slice(scandir($dir),2) ; // er moddhe dir er sob gaan rakha hoise 
 					$s="";
 					for ($i=0; $i<count($a) ; $i++) 
 			 		{   
 			 			$path_parts = pathinfo($a[$i]);
-			 			$name=$path_parts['basename'];
-			 				$mp3file = new MP3File("uploads/".$dirname."/".$a[$i]);
+			 			$name=$path_parts['basename']; // file name 
+			 				$mp3file = new MP3File("uploads/".$dirname."/".$a[$i]);//song path 
 			 				$duration2 = $mp3file->getDuration();
 			 				$dur=MP3File::formatTime($duration2);
 			 			$s=$i+1;
@@ -73,7 +122,7 @@
 			}
 		?>
 	</div>
-	<script src="https://code.jquery.com/jquery-2.2.0.js"></script>		
+	<script src="js/jquery-3.3.1.js"></script>		
 			<script>
 				audioPlayer();
 				function audioPlayer(){
---
+++ uploadsong.php
@@ -0,0 +1,74 @@
+<?php
+if (isset($_POST["submitb"]) && $_SERVER["REQUEST_METHOD"] == "POST")
+{
+$target_dir = "uploads/";//here the directory is already created manually
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);//base name provides filename with extension 
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION)); ///strlower(substr($name , strpos($name , ".")+1));
+// Check if image file is a actual image or fake image
+/*
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
+}
+*/
+// Check if file already exists
+if (file_exists($target_file)) {
+    echo "Sorry, file already exists.";
+    $uploadOk = 0;
+}
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 4000000000) {
+    echo "Sorry, your file is more than 4 mb .";
+    $uploadOk = 0;
+}
+// Allow certain file formats
+/*if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+    $uploadOk = 0;
+}*/
+// Check if $uploadOk is set to 0 by an error
+  require ("dbconnection.php");
+if ($uploadOk == 0) {
+    echo "Sorry, your file was not uploaded.";
+// if everything is ok, try to upload file
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file) && insert_a_song( basename( $_FILES["fileToUpload"]["name"]),$target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded. <br/>";
+       // echo "<img src=\"".$target_file."\" width =200  height =200 >" ;
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+}
+?>
+
+
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body align="center">
+	
+	<p> click here to upload songs </p>
+	<form method="POST" action=" <?php echo $_SERVER["PHP_SELF"]; ?>" enctype="multipart/form-data">
+	<input type="file" name="fileToUpload" accept=" .mp3 , audio/* ">
+	<br/>
+	<input type="submit" name="submitb" value="click">
+		
+	</form>
+
+
+</body>
+</html>
